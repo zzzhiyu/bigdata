13c35d8c3d9aaa6d9bbc35a7ece8318a2fefb729
杨志宇 <yangzy22091405@q1.com>
2022-11-29 14:52:13 +0800

    11/29 dict增加

--- go_redis/src/dict.go
+++ go_redis/src/dict.go
@@ -1,5 +1,11 @@
 package src
 
+const (
+	DICT_OK  = 0
+	DICT_ERR = 1
+	DICT_HT_INITIAL_SIZE = 4
+)
+
 type dicEntry struct {
 	key   interface{}
 	value interface{}
@@ -16,10 +22,36 @@ type dictHt struct {
 type dictType struct {
 	hashFunction func(key interface{}) uint32
 	keyDup       func(privData interface{}, key interface{}) interface{}
+	valueDup     func(privaData interface{}, value interface{}) interface{}
+	keyCompare   func(privaData interface{}, key1 interface{}, key2 interface{}) int
 }
 
 type dict struct {
+	dType      *dictType
 	privDate   interface{}
 	dictHts    [2]dictHt
 	treHashIdx int //rehash索引,rehash不在进行时，值为-1
 }
+
+type dictIterator struct {
+	d           *dict
+	table       int       //正在被迭代的哈希表号码 0、1
+	index       int       //迭代器当前所指向的哈希表索引位置
+	safe        int       //标识这个迭代是否安全
+	entry       *dicEntry //当前迭代到节点的指针
+	nextEntry   *dicEntry //当前迭代节点的下一个节点
+	fingerPrint int64
+}
+
+func DICT_NOTUSED(v interface{}) interface{} {
+	return v
+}
+
+func dictSetVal(d *dict, entry *dicEntry, _vale_ interface{})  {
+	if d.dType.valueDup != nil {
+		entry.value = d.dType.valueDup(d.privDate, _vale_)
+	} else {
+		entry.value = _vale_
+	}
+}
+
